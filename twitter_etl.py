8f826ff9ba705a6030293f7adad2adbe3f985202
Jonathan Buening <JoChrisMaBue@gmail.com>
2023-03-23 16:57:21 +0100

    Added hidden file containing keys

--- twitter_etl.py
+++ twitter_etl.py
@@ -3,3 +3,16 @@ import pandas as pd
 import json
 from datetime import datetime
 import s3fs
+import constants
+
+# Keys, stored in a separate file
+access_key = constants.ACCESS_KEY
+secret_key = constants.SECRET_KEY
+consumer_key = constants.CONSUMER_KEY
+consumer_secret = constants.CONSUMER_SECRET
+
+# Twitter aut
+auth = tweepy.OAuthHandler(access_key, secret_key)
+auth.set_access_token(consumer_key, consumer_secret)
+
+api = tweepy.API(auth)
